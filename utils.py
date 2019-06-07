9d1c6b66add3dd1cd78cd162697dce5be9420430
alissadb <alissa_bruijn@hotmail.com>
2019-06-07 11:19:21 +0200

    Change preprocessing function

--- utils.py
+++ utils.py
@@ -10,6 +10,18 @@ import shutil
 
 import torch
 
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import recall_score, precision_score, f1_score, classification_report
+import torch
+
+import re
+import contractions
+import emoji
+import string
+from nltk.tokenize import TweetTokenizer
+from wordcloud import STOPWORDS
+import numpy as np
+
 #Metrics
 def accuracy_recall_precision_f1(y_pred, y_target):
 
@@ -35,6 +47,71 @@ def accuracy_recall_precision_f1(y_pred, y_target):
 
     return accuracy, recall, precision, f1
 
+#Preprocess Data
+def clean_tweet(tweet, remove_punt_number_special_chars=False,remove_stopwords=False, apply_stemming=False):
+    """Clean tweets
+    Args:
+        tweet: (str) Tweet
+        remove_punt_number_special_chars: (bool) Remove punctuations, numbers and special characters
+        remove_stopwords: (bool) Remove stopwords
+        apply_stemming: (bool) Apply stemming on the words on the tweets
+    """
+    #Remove emojis
+    tweet = re.sub(":[a-zA-Z\-\_]*:","", emoji.demojize(tweet)) #:hear-no-evil_monkey:
+    tweet = re.sub(":\w+:","", emoji.demojize(tweet))
+    tweet = re.sub(":\w+\’\w+:","", emoji.demojize(tweet)) #:woman's_boot:
+
+    #Remove mentions, usernames (@USER)
+    tweet = re.sub("\s*@USER\s*", '', tweet)
+
+    #Remove URL
+    tweet = re.sub("\s*URL\s*", '', tweet)
+
+    #And
+    tweet = re.sub("&amp;", "and", tweet)
+    tweet = re.sub("&lt;", "<", tweet)
+    tweet = re.sub("&gt", ">", tweet)
+    tweet = re.sub("&", "and", tweet)
+
+    #Replace contractions and slang of word
+    tweet = re.sub("i'm", "I'm", tweet)
+    tweet = contractions.fix(tweet, slang=True)
+
+    #Lowercase
+    tweet = tweet.lower()
+
+    #Remove Hashtags + Words
+    tweet = re.sub("#\s*\w+\s*", '', tweet)
+
+    #Remove repeating whitespaces
+    tweet = re.sub("\s[2, ]"," ", tweet)
+
+    #Remove non ascii characters
+    tweet.encode("ascii", errors="ignore").decode()
+
+    #Remove punctuations, numbers and special characters (remove emoticons)
+    if remove_punt_number_special_chars:
+        tweet = re.sub('[^a-zA-Z]', ' ', tweet)
+
+    #Tokenize tweet
+    tt = TweetTokenizer(preserve_case=False,
+                    strip_handles=True,
+                    reduce_len=True)
+
+    tweet_tokens = tt.tokenize(tweet)
+
+    #Remove stopwords
+    if remove_stopwords:
+        stopwords = set(STOPWORDS)
+        tweet_tokens = [token for token in tweet_tokens if token not in stopwords]
+
+    #Stemming
+    if apply_stemming:
+        tweet_stem = [stemmer.stem(token) for token in tweet_tokens]
+
+    clean = " ".join(tweet_tokens)
+
+    return clean
 
 #BERT
 def convert_examples_to_features(data, max_seq_length, tokenizer):
@@ -50,23 +127,17 @@ def convert_examples_to_features(data, max_seq_length, tokenizer):
     features = pd.DataFrame(columns=col_names)
 
     for index, example in data.iterrows():
-        #print(example)
 
         tokens_tweet = tokenizer.tokenize(example.tweet)
-        #print(tokens_tweet)
 
         # Account for [CLS] and [SEP] with "- 2"
         if len(tokens_tweet) > max_seq_length - 2:
             tokens_tweet = tokens_tweet[:(max_seq_length - 2)]
 
         tokens = ["[CLS]"] + tokens_tweet + ["[SEP]"]
-        #print(tokens)
-        #print("Length tokens:", len(tokens))
         segment_ids = [0] * len(tokens)
-        #print(segment_ids)
-
         input_ids = tokenizer.convert_tokens_to_ids(tokens)
-        #print(input_ids)
+
         # The mask has 1 for real tokens and 0 for padding tokens. Only real tokens are attended to.
         input_mask = [1] * len(input_ids)
 
@@ -76,39 +147,13 @@ def convert_examples_to_features(data, max_seq_length, tokenizer):
         input_ids += padding
         input_mask += padding
         segment_ids += padding
-        #print(example.label)
+
         label_id = int(example.label)
-        #print(label_id)
 
         input_features = {'input_ids': input_ids, 'input_mask':input_mask, 'segment_ids':segment_ids, 'label_id':label_id}
         features.loc[len(features)] = input_features
 
     return features
-
-
-#Logger
-def set_logger(log_path):
-    #Source: https://github.com/cs230-stanford/cs230-code-examples/blob/master/pytorch/nlp/utils.py
-
-    """
-    Arguments:
-        log_path: Path to log
-    """
-
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-
-    if not logger.handlers:
-        # Logging to a file
-        file_handler = logging.FileHandler(log_path)
-        file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
-        logger.addHandler(file_handler)
-
-        # Logging to console
-        stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(logging.Formatter('%(message)s'))
-        logger.addHandler(stream_handler)
-
 
 #Checkpoints save and load
 def save_checkpoint(state, directory, checkpoint):
