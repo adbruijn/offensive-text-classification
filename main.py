803e9eff3f29e8fe191f5f5f69e8f60ab364ef6b
alissadb <alissa_bruijn@hotmail.com>
2019-07-05 12:21:20 +0200

    Change directory name

--- main.py
+++ main.py
@@ -216,13 +216,14 @@ def main(output_dim,
 
     id_nummer = f'{_run._id}'
 
-    directory = f"results-"+model_name.lower()+"/"+id_nummer+"/"
-    directory_checkpoints =  f"results-"+model_name.lower()+"/checkpoints"+"/"+id_nummer+"/"
-
     if "BERT" in model_name:  #Default = False, if BERT model is used then use_bert is set to True
         use_bert = True
+        directory = f"results-bert/"+model_name.lower()+"/"+id_nummer+"/"
+        directory_checkpoints =  f"results-bert/"+model_name.lower()+"/checkpoints"+"/"+id_nummer+"/"
     else:
         use_bert = False
+        directory = f"results-"+model_name.lower()+"/"+id_nummer+"/"
+        directory_checkpoints =  f"results-"+model_name.lower()+"/checkpoints"+"/"+id_nummer+"/"
 
     #Data
     if use_bert:
