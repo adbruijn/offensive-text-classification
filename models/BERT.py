af58157c2272a2c1a366c17740f48e29bd74e808
alissadb <alissa_bruijn@hotmail.com>
2019-07-30 11:59:38 +0200

    Change..

--- models/BERT.py
+++ models/BERT.py
@@ -74,7 +74,7 @@ class BertExtractEmbeddings(nn.Module):
             batch_token_sum_all.append(token_sum_all)
         sum_all = torch.stack(batch_token_sum_all)
 
-        return last_four_cat
+        return last_four_sum
 
     def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None):
 
